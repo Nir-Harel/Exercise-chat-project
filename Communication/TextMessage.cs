09d2ecc81d8e8b058553d78806def4750bc570c4
Nir-Harel <nirharelbenu@gmail.com>
2016-05-25 16:02:07 +0300

    Add files via upload

---
+++ Communication/TextMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Comunication
+{
+    [Serializable]
+    public abstract class TextMessage : BaseMessage
+    {
+        public string Text = "";
+        public string NickName { get; set;  }
+        public  Color Colors { get; set; }
+
+        public DateTime time = DateTime.Now;
+    }
+}
